187571b42676b56faa64e27b5e69c5f332f131f7
Jimmy <jimmy@Jimmys-MacBook-Pro.local>
2017-04-05 18:50:46 -0700

    create proj

---
+++ news/news/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  news
+//
+//  Created by Jimmy on 4/5/17.
+//  Copyright © 2017 teddy-jimmy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
